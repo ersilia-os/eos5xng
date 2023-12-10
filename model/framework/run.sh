65b1b73f756e7b91ed4be7dadb694a2656479b3d
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-10 18:29:08 +0100

    Update run.sh

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,4 +1,4 @@
 python $1/code/save_features.py --data_path $2 --save_path 'features.npz' --features_generator rdkit_2d_normalized
-python $1/code/predict.py --no_features_scaling --features_path 'features.npz' --test_path $2 --checkpoint_dir 'checkpoints/model6' $3 --preds_path $4
+python $1/code/predict.py --no_features_scaling --features_path 'features.npz' --test_path $2 --checkpoint_dir 'checkpoints/model6' $3 --preds_path 'example_output.csv' $4
 
 

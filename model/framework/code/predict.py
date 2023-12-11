9a3baaf1acea806cd5e6c31c4311300450ef7296
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-11 02:21:57 +0100

    Update predict.py

--- model/framework/code/predict.py
+++ model/framework/code/predict.py
@@ -1,19 +1,13 @@
 """Loads a trained chemprop model checkpoint and makes predictions on a dataset."""
 import os, sys
-import chemprop.train
 from chemprop.train import chemprop_predict
-from chemprop.args import PredictArgs
 
 #intermediate features file created
-root = os.path.dirname(os.path.abspath(__file__))
-features_file =  os.path.abspath(os.path.join(root, "..","features.npz"))
-checkpoint_dir = os.path.abspath(os.path.join(root,"..", "..","checkpoints", "model6"))
-#features_file = sys.argv[-1]
+features_file = sys.argv[-1]
 
 if __name__ == '__main__':
-    print("Now predicting")
+    print("now predicting")
     chemprop_predict()
-    print("Done predicting")
-    
-
-
+    print("done predicting")
+    #Remove intermediate features file after making predictions
+    os.remove(features_file)

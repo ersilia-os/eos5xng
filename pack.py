4fa970c25e516633136fe1ff21f463c763afdc71
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-11 02:29:58 +0100

    Update pack.py

--- pack.py
+++ pack.py
@@ -4,7 +4,7 @@ from src.service import Service
 from src.service import CHECKPOINTS_BASEDIR, FRAMEWORK_BASEDIR
 
 root = os.path.dirname(os.path.realpath(__file__))
-mdl = load_model(
+mdl = load_chemprop_model(
     os.path.join(root, "model", FRAMEWORK_BASEDIR),
     os.path.join(root, "model", CHECKPOINTS_BASEDIR),
 )

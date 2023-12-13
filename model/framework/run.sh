f478dfed35b773d2f81a99d518c827f47bd29bca
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-13 18:05:01 +0100

    Update run.sh

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,2 +1,3 @@
 python $1/code/save_features.py --data_path $2 --save_path TEMP_Feat --features_generator rdkit_2d_normalized
 python $1/code/predict.py --no_features_scaling --test_path $2 --checkpoint_dir  $1/../checkpoints --preds_path $3 --features_path TEMP_Feat.npz
+rm -r TEMP_Feat.npz
